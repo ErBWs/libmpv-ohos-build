1161f4565763cf7ff69ce2ddcd1487e3678fcc25
ErBW_s <erbws@foxmail.com>
2025-09-02 00:03:44 +0800

    add rust and some crossfile

---
+++ download/download-ohos-rs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -eu
+
+# Rust is already embedded in GitHub Action
+# curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
+
+rustup target add aarch64-unknown-linux-ohos
+cargo install ohrs
