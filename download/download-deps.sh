b921b86658298d3cf9e587724abfd5e52a520e3f
ErBW_s <erbws@foxmail.com>
2025-09-01 20:27:47 +0800

    chore: update git clone

--- download/download-deps.sh
+++ download/download-deps.sh
@@ -7,10 +7,10 @@ set -eu
 cd $HOME/libmpv
 
 # libass
-[ ! -d libass ] && git clone --depth 1 --branch $V_LIBASS https://github.com/libass/libass.git libass
+[ ! -d libass ] && git clone --depth 1 -b $V_LIBASS https://github.com/libass/libass.git libass
 
 # ffmpeg
-[ ! -d ffmpeg ] && git clone --depth 1 --branch $V_FFMPEG https://github.com/FFmpeg/FFmpeg.git ffmpeg
+[ ! -d ffmpeg ] && git clone --depth 1 -b $V_FFMPEG https://github.com/FFmpeg/FFmpeg.git ffmpeg
 
 # mpv
-[ ! -d mpv ]  && git clone --depth 1 --branch $V_MPV https://github.com/mpv-player/mpv.git mpv
+[ ! -d mpv ]  && git clone --depth 1 -b $V_MPV https://github.com/mpv-player/mpv.git mpv
